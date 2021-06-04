dd4c7f70c8695954ca34bf0c5073daee1257dd72
Mohammad <m.nazarizadeh@gmail.com>
2021-06-04 15:33:08 +0430

    comment added to card service

--- src/main/java/com/digipay/cardmanagement/service/CardService.java
+++ src/main/java/com/digipay/cardmanagement/service/CardService.java
@@ -6,10 +6,29 @@ import org.springframework.data.domain.Pageable;
 
 public interface CardService {
 
+    /**
+     * create a card for merchant which requested for it
+     *
+     * @param merchantId    the ID of merchant calling the action
+     * @param cardDTO       the card properties
+     * @return              a card which is created
+     */
     CardDTO createCard(Long merchantId, CardDTO cardDTO);
 
+    /**
+     * get merchant cards with pagination
+     *
+     * @param merchantId    the ID of merchant calling the action
+     * @param pageable      pagination properties
+     * @return              a page of cards
+     */
     Page<CardDTO> getMerchantCards(Long merchantId, Pageable pageable);
 
+    /**
+     * deactivate a card of merchant
+     * @param merchantId    the ID of merchant calling the action
+     * @param cardId        the ID of card to be deactivated
+     */
     void softDelete(Long merchantId, Long cardId);
 
 }
